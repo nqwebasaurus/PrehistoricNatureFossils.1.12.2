18b613a6776c3ebcfe9dd30d5d00cf8b6510a7af
AechtRob <Aecht_Rob@outlook.com>
2023-06-29 17:39:51 +0100

    v.2 Version number

--- src/main/java/net/prehistoricnaturefossils/PrehistoricNatureFossils.java
+++ src/main/java/net/prehistoricnaturefossils/PrehistoricNatureFossils.java
@@ -27,7 +27,7 @@ import org.apache.logging.log4j.Logger;
 public class PrehistoricNatureFossils {
     public static final String MODID = "prehistoricnaturefossils";
     public static final String NAME = "Prehistoric Nature Fossils";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "2.0";
     public static final String MCVERSION = "[1.12.2]";
     public static final Logger LOGGER = LogManager.getLogger("prehistoricnaturefossils");
     public static final CreativeTabs CREATIVE_TAB = new CreativeTabFossils();
