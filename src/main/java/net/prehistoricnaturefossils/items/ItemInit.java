1b2a425be61611318c8c613e454711db3e0275b6
nqwebasaurus <blackfrog@hotmail.it>
2024-05-15 21:13:18 +0200

    Advancement tree refactor + skull icons

--- src/main/java/net/prehistoricnaturefossils/items/ItemInit.java
+++ src/main/java/net/prehistoricnaturefossils/items/ItemInit.java
@@ -1,6 +1,8 @@
 package net.prehistoricnaturefossils.items;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.Item;
+import net.prehistoricnaturefossils.blocks.slabs.BlockFossilAkmonistion;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,5 +10,7 @@ import java.util.List;
 public class ItemInit {
 
     public static final List<Item> ITEMS = new ArrayList<Item>();
+
+    public static final Item FOSSIL_ADVANCEMENT = new ItemAdvancement();
 
 }
